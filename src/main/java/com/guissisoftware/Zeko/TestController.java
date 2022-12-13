1786a6dcd8991f360d99a25237b422759b459389
Henri-joel Guissi <guissi.henrijoelzeze@bluecrest.edu.gh>
2022-12-13 18:23:41 +0000

    Setting up database connection and testing the driver by creating a table. 👽⚡✔

--- src/main/java/com/guissisoftware/Zeko/TestController.java
+++ src/main/java/com/guissisoftware/Zeko/TestController.java
@@ -1,5 +1,13 @@
 package com.guissisoftware.Zeko;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello Test";
+    }
 
 }
---
+++ src/main/java/com/guissisoftware/Zeko/TestDataObject.java
@@ -0,0 +1,14 @@
+package com.guissisoftware.Zeko;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestDataObject {
+    @Id
+    private long id;
+    @Column
+    private String username;
+
+}
